f0e3a0caf9c362bfef49e40e1c5c366ac107b3af
Gymnastiar Ramadhan <92432242+GymnastiarR@users.noreply.github.com>
2023-10-16 23:36:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-// import app from './server.js';
+import app from './server.js';
 import env from 'dotenv';
-// import server from './server.js';
-// import task from './cron.js';
+import task from './cron.js';
+
 // app.get( '/rfid/:id/assign', ( req, res, next ) => {
 //     io.emit( 'RFID', req.params.id );
 //     res.status( 200 ).json( { message: "Hello World" } );
 // } );
 
-import express from 'express';
-
-const server = new express();
-
-server.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).json({ status: "OK" });
 });
 
 env.config();
 
-server.listen( process.env.PORT, () => {
+app.listen( process.env.PORT, () => {
     console.log( "Server is running on port " + process.env.PORT + "." );
 } );
 
