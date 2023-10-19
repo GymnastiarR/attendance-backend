da0ee16dc8ea0086e18c3baef27fdbe9237c3f05
=GymnastiarR <=>
2023-10-20 02:43:38 +0700

    test

--- controllers/attandanceController.js
+++ controllers/attandanceController.js
@@ -168,13 +168,30 @@ export const updateAttendance = async ( req, res, next ) => {
         // const { token } = req.query;
         // if ( !token ) throw new Error( "Token Tidak Ditemukan" );
 
-        const attendance = await prisma.attendance.findUnique( {
-            where: {
-                date: `${moment().format( "YYYY-MM-DD" )}T00:00:00.000Z`
-            }
-        } );
+        const attendance = await prisma.attendance.findMany();
+        const date = `${moment().format( "YYYY-MM-DD" )}T00:00:00.000Z`;
 
-        res.status( 200 ).json( { message: "Berhasil", data: attendance } );
+
+        // const attendance = await prisma.attendance.update( {
+        //     where: {
+        //         date: `${moment().format( "YYYY-MM-DD" )}T00:00:00.000Z`
+        //     },
+        //     data: {
+        //         AttendanceStudent: {
+        //             updateMany: {
+        //                 where: {
+        //                     studentId: student.id
+        //                 },
+        //                 data: {
+        //                     status: "Hadir",
+        //                     datePresence: moment().format()
+        //                 }
+        //             }
+        //         }
+        //     }
+        // } );
+
+        res.status( 200 ).json( { message: "Berhasil", data: { attendance, date } } );
     } catch ( error ) {
         next( error );
     }
